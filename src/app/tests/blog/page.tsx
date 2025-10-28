4e6e6a801ec72f4677034d6a7aa2a8073adb26e8
Magnno <anderson956m@gmail.com>
2025-10-28 02:02:52 -0300

    feat: Configura a fonte base e adiciona rota de teste (/tests/blog) para visualização

---
+++ src/app/tests/blog/page.tsx
@@ -0,0 +1,54 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen flex justify-center items-center p-2">
+      <div className="flex flex-col max-w-169 gap-2">
+        <h1>Exploring the mysteries of Atlantis</h1>
+        <small>Oct 28th, 2025 — By Seraphina </small>
+        <p>
+          Atlantis, the Lost City of Myth and Legend, has captivated the human
+          imagination for centuries. In this article, we will delve into the
+          depths of this enigmatic sunken realm and uncover the secrets hidden
+          beneath the waves.
+        </p>
+        <h2>The origins of Atlantis</h2>
+        <p>
+          The first mention of Atlantis can be traced back to the works of the
+          ancient Greek philosopher Plato. He described a powerful and advanced
+          civilization that disappeared beneath the ocean’s surface, leaving
+          behind only speculation and intrigue. But what if Atlantis was more
+          than just a legend?
+        </p>
+        <h3>Theories and speculations</h3>
+        <p>
+          Over the years, numerous theories and speculations have arisen
+          regarding the existence and fate of Atlantis. Some believe it was a
+          highly advanced society with technology far beyond its time, while
+          others argue that it was purely a product of Plato’s imagination.
+          Let’s explore some of these intriguing ideas.
+        </p>
+        <h4>Advanced technology</h4>
+        <p>
+          One theory suggests that Atlantis possessed technology that allowed it
+          to harness the Earth’s energy, leading to its eventual downfall.
+          Theorists propose that their unchecked power and environmental
+          exploitation may have contributed to their watery demise.
+        </p>
+        <h5>Geological evidence</h5>
+        <p>
+          Geological formations and underwater structures have led some
+          researchers to believe they may have found the remnants of Atlantis.
+          Could these enigmatic formations on the ocean floor be the lost city
+          we’ve been searching for?
+        </p>
+        <h6>Conclusion</h6>
+        <p>
+          In conclusion, the mystery of Atlantis continues to intrigue and
+          baffle historians, archaeologists, and enthusiasts alike. Whether
+          Atlantis was a real, technologically advanced civilization or a mere
+          figment of Plato’s imagination remains an unsolved riddle waiting to
+          be unraveled.
+        </p>
+      </div>
+    </div>
+  );
+}
